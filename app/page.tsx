f930e39734fb2670fb09783258755a40489140a7
Yurii Paraska <paraska108@gmail.com>
2024-09-22 20:33:56 +0300

    feat: add categories

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Categories } from '@/components/shared/categories';
 import { Container } from '@/components/shared/container';
 import { Header } from '@/components/shared/header';
 import { Title } from '@/components/shared/title';
@@ -6,6 +7,12 @@ export default function Home() {
   return (
     <div>
       <Header />
+
+      <Container className='mt-5'>
+        <Title text='All pizzas' size='lg' className='font-extrabold' />
+
+        <Categories />
+      </Container>
     </div>
   );
 }
