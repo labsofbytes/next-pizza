dc35954c65a230b2f7c0e847427d7bbf71158460
Yurii Paraska <paraska108@gmail.com>
2024-11-04 12:13:56 +0200

    feat: add ingredients to item pizza cards

--- shared/components/shared/product-card.tsx
+++ shared/components/shared/product-card.tsx
@@ -4,6 +4,7 @@ import { Plus } from 'lucide-react';
 import { cn } from '@/shared/lib/utils';
 import { Title } from './title';
 import Link from 'next/link';
+import { Ingredient } from '@prisma/client';
 // import { CountButton } from './count-button';
 
 interface Props {
@@ -12,10 +13,11 @@ interface Props {
   price: number;
   count?: number;
   imageUrl?: string;
+  ingredients: Ingredient[];
   className?: string;
 }
 
-export const ProductCard: React.FC<Props> = ({ name, price, count, imageUrl, id, className }) => {
+export const ProductCard: React.FC<Props> = ({ name, price, count, imageUrl, id, ingredients, className }) => {
   return (
     <Link href={`/product/${id}`} className={cn(className)}>
       <div className='flex justify-center p-6 bg-secondary rounded-lg h-[260px]'>
@@ -23,7 +25,12 @@ export const ProductCard: React.FC<Props> = ({ name, price, count, imageUrl, id,
       </div>
       <Title text={name} size='sm' className='mb-1 mt-3 font-bold' />
       <p className='text-sm text-gray-400'>
-        Chicken, mozzarella, cheddar and parmesan cheese, cheese sauce, tomatoes, alfalfa sauce, garlic
+        {ingredients.map((ingredient, index) => (
+          <span key={index}>
+            {ingredient.name}
+            {index < ingredients.length - 1 && ', '}
+          </span>
+        ))}
       </p>
 
       <div className='flex justify-between items-center mt-4'>
--- shared/components/shared/product-group-list.tsx
+++ shared/components/shared/product-group-list.tsx
@@ -6,10 +6,11 @@ import { useIntersection } from 'react-use';
 
 import { Title } from './title';
 import { ProductCard } from './product-card';
+import { ProductWithRelations } from '@/@types/prisma';
 
 interface Props {
   title: string;
-  items: any[];
+  items: ProductWithRelations[];
   categoryId: number;
   className?: string;
 }
@@ -39,6 +40,7 @@ export const ProductsGroupList: React.FC<Props> = ({ title, items, className, ca
             imageUrl={item.imageUrl}
             price={item.items[0].price}
             count={index % 2}
+            ingredients={item.ingredients}
           />
         ))}
       </div>
